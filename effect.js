28c7d44cb40094c2f72821a196489cf48a2e4a0d
binh1762 <tranbinh1762@gmail.com>
2024-06-08 17:09:09 +0700

    init

--- effect.js
+++ effect.js
@@ -101,7 +101,6 @@ $('document').ready(function(){
 			loopSeven();
 		});
 	}
-
 	$('#balloons_flying').click(function(){
 		$('.balloon-border').animate({top:-500},8000);
 		$('#b1,#b4,#b5,#b7').addClass('balloons-rotate-behaviour-one');
@@ -140,9 +139,9 @@ $('document').ready(function(){
 
 		
 	$('#wish_message').click(function(){
-		 vw = $(window).width()/2;
+		 vw = $(window).width();
 
-		$('#b1,#b2,#b3,#b4,#b5,#b6,#b7').stop();
+		$('#b1,#b2,#b3,#b4,#b5,#b6,#b7,#c1,#c2,#c3,#c4,#c5,#c6,#c7').stop();
 		$('#b1').attr('id','b11');
 		$('#b2').attr('id','b22')
 		$('#b3').attr('id','b33')
